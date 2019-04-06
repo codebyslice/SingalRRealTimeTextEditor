6ade9ba31d2eab7ed9fc39328ff711d2bdad7a0b
Anirudh Samudrala <anirudh.enigma@gmail.com>
2019-04-06 01:23:42 -0700

    text change

--- TextApp/TextHub.cs
+++ TextApp/TextHub.cs
@@ -9,7 +9,7 @@ namespace TextApp
     public class TextHub:Hub
     {
         public void Text(string message)
-        {
+         {
             //The method mentioned here will be the method on the client that SingalR looks for to talk to
             Clients.All.SendAsync("Send", message);
         }
